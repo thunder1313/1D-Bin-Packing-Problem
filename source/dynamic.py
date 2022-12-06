73eea17cab58e7f11b9401e02f9b6e6988f35f4b
thunder1313 <maks.pata@gmail.com>
2022-12-06 13:08:15 +0100

    remove dynamic bruuh

--- source/dynamic.py
+++
@@ -1,7 +0,0 @@
-def dynamic(problem, i, l):
-    if i == -1 or l == 0:
-        return 0
-    if problem[0][i] > l:
-        return function(problem, i-1, l)
-    if problem[0][i] <= l:
-        return max(function(problem, i-1, l), function(problem, i-1, l-problem[0][i]) + problem[1][i])
