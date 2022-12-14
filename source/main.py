8c5d50eb82fdf850f9e77f05050370d5cee2c34f
thunder1313 <maks.pata@gmail.com>
2022-12-14 16:32:46 +0100

    simplify

--- source/main.py
+++ source/main.py
@@ -6,43 +6,9 @@ from bestFit import bestFit
 from firstFitDecreasing import firstFitDecreasing
 from bestFitDecreasing import bestFitDecreasing
 
-from time import time
-
-# print("items: ", items)
 print("optimal: ", used_bins)
-# print("bin capacity: ", bin_capacity)
 print("Next Fit: ", nextFit(items, bin_capacity))
 print("First Fit: ", firstFit(items, bin_capacity))
 print("Best Fit: ", bestFit(items, bin_capacity))
 print("First Fit Decreasing: ", firstFitDecreasing(items, bin_capacity))
 print("Best Fit Decreasing: ", bestFitDecreasing(items, bin_capacity))
-# start = time()
-# print(nextFit(items, bin_capacity))
-# end = time()
-# t1 = end - start
-
-# start = time()
-# print(firstFit(items, bin_capacity))
-# end = time()
-# t2 = end - start
-
-# start = time()
-# print(bestFit(items, bin_capacity))
-# end = time()
-# t3 = end - start
-
-# start = time()
-# print(firstFitDecreasing(items, bin_capacity))
-# end = time()
-# t4 = end - start
-
-# start = time()
-# print(bestFitDecreasing(items, bin_capacity))
-# end = time()
-# t5 = end - start
-
-# print(t1)
-# print(t2)
-# print(t3)
-# print(t4)
-# print(t5)
